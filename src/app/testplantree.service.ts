dc18ef472ed4ad9e0818abd383ab08b53e740fd0
Hoang Nguyen Thai <thaihoang.nguyen@EITSVN.COM>
2019-10-01 16:57:03 +0700

    Fix testcase testgroup

--- src/app/testplantree.service.ts
+++ src/app/testplantree.service.ts
@@ -7,6 +7,9 @@ import { Observable } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { BaseService } from './base.service';
 import { Category } from './classes/category';
+import { map } from 'rxjs/operators';
+import { plainToClass } from 'class-transformer';
+import { TreeNode } from 'primeng/components/common/treenode';
 
 @Injectable({
   providedIn: 'root'
@@ -26,5 +29,44 @@ export class TestplantreeService extends BaseService {
         .set('authorization', localStorage.token)
     }
     return this.http.get<Category[]>(`${this.url}/categories/testsuites/processed`, headers)
+      .pipe(
+        map(response => {
+          return plainToClass(Category, response)
+        })
+      )
+  }
+  getSelectedNode(treeNode): any {
+    let url = `${this.url}/`
+    let headers = {
+      headers: new HttpHeaders()
+        .set('authorization', localStorage.token)
+    }
+    console.log(treeNode.type)
+    switch (treeNode.type) {
+      case "category":
+        url += `categories/${treeNode.id}`
+        return this.http.get<Category>(`${url}`, headers)
+        .pipe(
+          map(response => {
+            return plainToClass(Category, response)
+          })
+        )
+      case "testsuite":
+        url += `testsuites/${treeNode.id}`
+        return this.http.get<TestSuite>(`${url}`, headers)
+        .pipe(
+          map(response => {
+            return plainToClass(TestSuite, response)
+          })
+        )
+      case "testgroup":
+        url += `testgroups/${treeNode.id}`
+        break;
+      case "testcase":
+        url += `testcases/${treeNode.id}`
+        break;
+    }
+    
+
   }
 }
--- src/app/testplantree/testplantree.component.ts
+++ src/app/testplantree/testplantree.component.ts
@@ -1,13 +1,12 @@
-import { Component, OnInit, ViewChild, AfterViewInit, ChangeDetectorRef } from '@angular/core';
-import { TREE_ACTIONS, KEYS, IActionMapping, ITreeOptions } from 'angular-tree-component';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { Category } from '../classes/category';
 
 import { TestplantreeService } from '../testplantree.service';
-import { map } from 'rxjs/operators';
-import { plainToClass } from "class-transformer";
-import { HttpClient } from '@angular/common/http';
-import { TestPlan } from '../classes/testplan';
-import { MatMenuTrigger } from '@angular/material/menu';
+import { TreeNode } from 'primeng/components/common/treenode';
+import { TestCase } from '../classes/testcase';
+import { TestGroup } from '../classes/testgroup';
+import { TestSuite } from '../classes/testsuite';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-testplantree',
@@ -15,9 +14,13 @@ import { MatMenuTrigger } from '@angular/material/menu';
   styleUrls: ['./testplantree.component.css'],
 })
 export class TestplantreeComponent implements OnInit {
-  @ViewChild('menu', { static: false }) trigger: MatMenuTrigger;
   filterValue: string = ''
   categories: Category[] = []
+  testPlanTree: any[] = []
+  loading: boolean = true
+  loadingIcon: string = "fa-circle-o-notch"
+  selectedNode: TreeNode[]
+  selectedObject: TestCase | TestGroup | TestSuite | Category
   items = [
     { label: 'View', icon: 'fa fa-search', command: () => {}},
     { label: 'Unselect', icon: 'fa fa-close', command: () => {}}
@@ -27,44 +30,62 @@ export class TestplantreeComponent implements OnInit {
     private testplanService: TestplantreeService,
     private cdr: ChangeDetectorRef) {
   }
-  ngOnInit(): void {
-    this.testplanService.getCategories()
-      .pipe(
-        map(response => {
-          const testPlanResult = plainToClass(Category, response)
-          return testPlanResult.map((cat) => ({
-            ...cat,
-            id: cat._id,
-            label: cat.name,
-            "expandedIcon": `pi pi-folder-open ${cat.status.toLowerCase()}`,
-            "collapsedIcon": `pi pi-folder ${cat.status.toLowerCase()}`,
-            children: cat.testSuites.map((ts) => ({
-              ...ts,
-              id: ts._id,
-              label: ts.name,
-              "expandedIcon": `pi pi-align-justify ${ts.status.toLowerCase()}`,
-              "collapsedIcon": `pi pi-align-justify ${ts.status.toLowerCase()}`,
-              children: ts.testGroups.map((tg) => ({
-                ...tg,
-                id: tg._id,
-                label: tg.name,
-                "expandedIcon": `pi pi-clone ${tg.status.toLowerCase()}`,
-                "collapsedIcon": `pi pi-clone ${tg.status.toLowerCase()}`,
-                children: tg.testCases.map((tc) => ({
-                  ...tc,
-                  id: tc._id,
-                  label: tc.name,
-                  "expandedIcon": `pi pi-file-o ${tc.status.toLowerCase()}`,
-                  "collapsedIcon": `pi pi-file-o ${tc.status.toLowerCase()}`,
-                }))
-              }))
+  onNodeSelect(event){
+    console.log('event', event.node)
+    this.testplanService.getSelectedNode(event.node).subscribe(node => {
+      this.selectedObject = node
+      console.log('selectedObject', this.selectedObject)
+    })
+  }
+
+  mapToTree(cats: Category[]): any {
+    return cats.map((cat) => ({
+      ...cat,
+      id: cat._id,
+      label: cat.name,
+      expandedIcon: `pi pi-folder-open ${cat.status.toLowerCase()}`,
+      collapsedIcon: `pi pi-folder ${cat.status.toLowerCase()}`,
+      children: cat.testSuites.map((ts) => ({
+        ...ts,
+        id: ts._id,
+        label: ts.name,
+        expandedIcon: `pi pi-align-justify ${ts.status.toLowerCase()}`,
+        collapsedIcon: `pi pi-align-justify ${ts.status.toLowerCase()}`,
+        children: _.flatten([
+          ts.testCases.map((tcc) => <any>({
+            ...tcc,
+            id: tcc._id,
+            label: tcc.name,
+            expandedIcon: `pi pi-file-o ${tcc.status.toLowerCase()}`,
+            collapsedIcon: `pi pi-file-o ${tcc.status.toLowerCase()}`,
+          })),
+          ts.testGroups.map((tg) => ({
+            ...tg,
+            id: tg._id,
+            label: tg.name,
+            expandedIcon: `pi pi-clone ${tg.status.toLowerCase()}`,
+            collapsedIcon: `pi pi-clone ${tg.status.toLowerCase()}`,
+            children: tg.testCases.map((tc) => ({
+              ...tc,
+              id: tc._id,
+              label: tc.name,
+              expandedIcon: `pi pi-file-o ${tc.status.toLowerCase()}`,
+              collapsedIcon: `pi pi-file-o ${tc.status.toLowerCase()}`,
             }))
-          }));
-        }),
-      ).subscribe((list: any[]) => {
-        this.categories = list;
-        this.cdr.detectChanges();
-        console.log(this.categories)
-      })
+          }))
+        ])
+      }))
+    })); 
+  }
+
+  ngOnInit(): void {
+    this.testplanService.getCategories().subscribe(categories => {
+      this.categories = categories
+      console.log(this.categories)
+      this.testPlanTree = this.mapToTree(this.categories)  
+      this.loading = false
+      this.cdr.detectChanges()
+      console.log(this.testPlanTree)
+    })
   }
 }
